216e7fef806d8c51eb97d8a94ecaa14d586cccd0
Olivier El Mekki <olivier@el-mekki.com>
2013-08-31 16:25:03 +0200

    REFACTOR tests on both rails-3 and rails-4
    It is now possible to run tests against both rails-3 and rails-4 :
    
    Rails-4 :
    
    ```shell
     # One time setup
    bundle install --gemfile Gemfile.rails4
    cd test/dummy_rails4
    BUNDLE_GEMFILE=../../Gemfile.rails4 bundle exec rake db:migrate
    BUNDLE_GEMFILE=../../Gemfile.rails4 bundle exec rake db:test:prepare
    cd ../..
    
     # Then
    bundle exec rake test
    ```
    
    Rails-3 :
    
    ```shell
     # One time setup
    bundle install --gemfile Gemfile.rails3
    cd test/dummy_rails3
    BUNDLE_GEMFILE=../../Gemfile.rails3 bundle exec rake db:migrate
    BUNDLE_GEMFILE=../../Gemfile.rails3 bundle exec rake db:test:prepare
    cd ../..
    
     # Then
    RAILS_VERSION=3 bundle exec rake test
    ```
    
    Close #8

---
+++ test/dummy_rails4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'a3aba6b58c9b07a6d4b7b3d40a8372530d464365be5f4e182c42e84de68b1d688d5dc544420dd9dfc7abffd1888b73627b4dfdccc2cb2e6392ce5ab41ac7db1d'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+dummy_app = ENV[ 'RAILS_VERSION' ] == '3' ? 'dummy_rails3' : 'dummy_rails4'
+
+require File.expand_path("../#{dummy_app}/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
 Rails.backtrace_cleaner.remove_silencers!
@@ -10,6 +12,4 @@ Rails.backtrace_cleaner.remove_silencers!
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.method_defined?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-end
+ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
